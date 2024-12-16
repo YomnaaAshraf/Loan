997b501fdd325d03a9394d5ca92fa747a036b84a
YomnaaAshraf <106830883+YomnaaAshraf@users.noreply.github.com>
2024-12-16 14:39:25 +0200

    Update notebookb9c97f0c30.py

--- notebookb9c97f0c30.py
+++ notebookb9c97f0c30.py
@@ -320,7 +320,11 @@ model.fit(X_train, y_train)
 plt.figure(figsize=(15, 10))
 plot_tree(model, feature_names=X_train.columns, filled=True)
 plt.show()
+save_path = '/kaggle/working/ML_Model.pkl'
+with open(save_path, 'wb') as file:
+    pickle.dump(model, file)
 
+print(f"Model saved to {save_path}")
 # Model 2
 # Naive Bayes
 
